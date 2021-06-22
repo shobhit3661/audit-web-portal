f4705ca181a48e1474bedc28641d4e14ee5c3679
Shobhit Sharma <shobhitsharma.3661@gmail.com>
2021-06-22 17:30:55 +0530

    final

--- src/main/java/com/cognizant/AuditWebPortalApplication.java
+++ src/main/java/com/cognizant/AuditWebPortalApplication.java
@@ -2,13 +2,14 @@ package com.cognizant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
  // This is Application class for Spring boot
  
 @SpringBootApplication
 @EnableFeignClients
-public class AuditWebPortalApplication {
+public class AuditWebPortalApplication extends SpringBootServletInitializer {
 
 	
 	public static void main(String[] args) {
